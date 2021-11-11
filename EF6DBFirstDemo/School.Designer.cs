1df0d9ca3660a27fb15863f6ee47ff55e8e8404c
Rui Amorim <rui.m.amorim@gmail.com>
2021-11-11 09:24:23 +0000

    Update connection string

--- EF6DBFirstDemo/School.Designer.cs
+++ EF6DBFirstDemo/School.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\EntityFramework\EFProjects\EF6DBFirstDemo\EF6DBFirstDemo\School.edmx'. 
+﻿// T4 code generation is enabled for model 'P:\Repos\Develop\EntityFrameworkTutorial\EF6-DBFirst-Demo\EF6DBFirstDemo\School.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
